d765fd21b272aab62e55b22b1972924e340ce573
f-lab-steve <steve@f-lab.kr>
2022-07-28 21:50:29 -0700

    Leetcode 287

---
+++ src/com/steve/leetcode/Leetcode287.java
@@ -0,0 +1,18 @@
+package com.steve.leetcode;
+
+public class Leetcode287 {
+
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            for (int i = 0; i < nums.length; i++) {
+                int num = Math.abs(nums[i]);
+                if (nums[num - 1] < 0) {
+                    return num;
+                }
+                nums[num - 1] = -nums[num - 1];
+            }
+            return -1;
+        }
+    }
+
+}
