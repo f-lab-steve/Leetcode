92fa6b52a8bcab2744b8a76b53b9c43860e97659
f-lab-steve <steve@f-lab.kr>
2022-07-31 21:38:48 -0700

    Leetcode 136

---
+++ src/com/steve/leetcode/Leetcode136.java
@@ -0,0 +1,15 @@
+package com.steve.leetcode;
+
+public class Leetcode136 {
+
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int xor = 0;
+            for (int num : nums) {
+                xor ^= num;
+            }
+            return xor;
+        }
+    }
+
+}
